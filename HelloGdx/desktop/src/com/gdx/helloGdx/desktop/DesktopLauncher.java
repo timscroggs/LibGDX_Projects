1253d5bfc13349b029aacd227c9c920f180dbfb4
Tim Scroggs <tscroggs@gmail.com>
2015-03-04 10:05:06 -0500

    add files from macbook

---
+++ HelloGdx/desktop/src/com/gdx/helloGdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.helloGdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.helloGdx.HelloGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloGdx(), config);
+	}
+}
---
+++ tutorial_nightly2/desktop/src/com/hnycmbcreative/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hnycmbcreative.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hnycmbcreative.tutorial_nightly2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new tutorial_nightly2(), config);
+	}
+}
